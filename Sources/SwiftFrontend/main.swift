50f4f02f8545f641bb21efd42d5a15a770f7dca3
Alex Zenla <alex@fork.run>
2024-08-03 23:28:29 -0700

    SDL3 support

--- Sources/SwiftFrontend/main.swift
+++ Sources/SwiftFrontend/main.swift
@@ -1,4 +1,9 @@
-import CppBackend
+import Foundation
+import SDL3
 
-backend_init()
-print("Hello World")
+guard SDL_Init(SDL_INIT_VIDEO) >= 0 else {
+    print("SDL init failed.")
+    exit(0)
+}
+print("SDL init success.")
+SDL_Quit()
